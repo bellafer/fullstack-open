3bb79f21a6994ed1143c55f81e90a9af0887d029
Bea Llamas <bellafer@gmail.com>
2025-04-29 20:45:30 +0200

    Execices 1.4 & 1.5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,166 @@
+//1.4 INFORMACIÓN DEL CURSO PARTE 5
+/* 
+Llevemos los cambios un paso más allá. Cambia el curso y sus partes a un solo objeto JavaScript.
+Arregla todo lo que se rompa.
+*/
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development by Bellafer 1.5',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <p>
+        {course.parts[0].name}, exercises: {course.parts[0].exercises}
+      </p>
+      <p>
+        {course.parts[1].name}, exercises: {course.parts[1].exercises}
+      </p>
+      <p>
+        {course.parts[2].name}, exercises: {course.parts[2].exercises}
+      </p>
+      <p>Total: {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+export default App;
+
+//1.4 INFORMACIÓN DEL CURSO PARTE 4
+/* 
+Coloca los objetos en un array. 
+Modifica las definiciones de las variables de App de la siguiente forma y modifica las otras 
+partes de la aplicación que sean necesarias para que continue funcionando
+Nota: en este punto puedes asumir que siempre hay tres elementos,
+por lo que no es necesario pasar por los arrays usando bucles.
+
+Sin embargo, no pases diferentes objetos como props separados del componente App a los 
+componentes Content y Total. En su lugar, pásalos directamente como un array
+
+
 const Header = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = ({contenido}) => {
+  console.log(contenido);
+
+  return(
+    <div>
+      {contenido.map( (part, index) => (
+        <p key={index}>
+          {part.name}: {part.exercises}
+        </p>
+      )
+      )}
+    </div>
+  )
+}
+
+const Total = ({sumando}) => {
+  console.log(sumando);
+  const total = sumando.reduce((sum, parte) => sum + parte.exercises, 0);
+
+  return <p>Total: {total}</p>
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development by Bellafer 1.4';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  console.log(parts);
+  //console.log(parts[0].exercises);
+
+  return (
+    <div>
+      <Header course = {course} />
+      <Content contenido={parts} />
+      <Total sumando={parts}/>
+    </div>
+  )
+}
+
+export default App;
+
+*/
+
+
+// 1.3 INFORMACIÓN DEL CURSO PARTE 3
+/*Avancemos para usar objetos en nuestra aplicación. 
+Modifica las definiciones de las variables del componente App de la siguiente manera y 
+también refactoriza la aplicación para que siga funcionando
+
+const App = () => {
+  const course = "Half Stack application development";
+
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  }
+
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  }
+
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>{part1.name} {part1.exercises}</p>
+      <p>{part2.name} {part2.exercises}</p>
+      <p>{part3.name} {part3.exercises}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+    </div>
+  )
+
+}
+
+export default App;*/
+
+
+/*const Header = (props) => {
   console.log(props);
 
   return (
@@ -51,7 +213,7 @@ const Part = () => {
   )
 }*/
 
-const Content = (props) => {
+/*const Content = (props) => {
   console.log(props);
   
   return (
@@ -75,7 +237,7 @@ const App = () => {
   
   /*
   const total = [{content[0].exercises} + {content[1].exercises} + {content[2].exercises}]*/
-  const course = 'Half Stack application development';
+  /*const course = 'Half Stack application development';
 
   const part1 = 'Fundamentals of React';
   const exercises1 = 10;
@@ -92,7 +254,7 @@ const App = () => {
     </div>
   )
 }
-export default App
+export default App*/
 
 
 /*const Header = () => {
