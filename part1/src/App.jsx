0d62c82e02bf97b5ba9d3e17e6447c1b459ed416
Bea Llamas <bellafer@gmail.com>
2025-05-22 19:05:15 +0200

    Estado complejo, Manejo de arrays, Actualización de estado asíncrona y Renderizado condicional

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,20 +1,133 @@
 import { useState } from "react"; //Se importa la función useState
 
-const Display = (props) => {
+// ESTADO COMPLEJO
+/*const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+
+    setTotal(left + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
+    </div>
+  )
+}*/
+
+//RENDERIZADO CONDICIONAL.
+
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return(
+      <div>
+        The app is used by pressing the buttons.
+      </div>
+    )
+  }
   return(
-    <div>{props.counter}</div>
+    <div>
+      Button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
-const Button = (props) => {
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+
+    setTotal(left + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='Left' />
+      <Button handleClick={handleRightClick} text='Right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const Display = (props) => {
+  return(
+    <div>{props.counter}</div>
+  )
+}*/
+//simplificando el componente DISPLAY
+//const Display = ({counter}) => <div>{counter}</div>;
+
+/*const Button = (props) => {
   return(
     <button onClick={props.onClick}>
       {props.text}
     </button>
   )
-}
+}*/
+//simplificando el componente BUTTON
+/*const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)*/
 
-const App = () => {
+//una sola línea: - onSmash es válido, es otra forma de nombrar
+//const Button = ({onSmash, text}) => <button onClick={onSmash}>{text}</button>
+
+/*const App = () => {
   const [counter, setCounter] = useState(0); // Se llama a la función asignando el valor inicinal a 0
   console.log('renderizando con counter value', counter);
 
@@ -51,7 +164,7 @@ const App = () => {
 }
 
 export default App;
-
+*/
 /*
 //Re-renderizado de la página
 const App = (props) => {
